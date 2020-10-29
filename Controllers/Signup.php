87a553f41a334100cabed32c3a13cbc6e1867003
Dimuthu Lakmal <39994919+Lakmal98@users.noreply.github.com>
2020-10-29 13:31:35 +0530

    minor fix

--- Controllers/Signup.php
+++ Controllers/Signup.php
@@ -57,7 +57,7 @@ class Signup extends BaseController {
                             $hash = bin2hex(random_bytes(32));
                             $stmt = DB::execute(Hash::save(['user_id' => $userId, 'hash' => $hash]));
 
-                            http_response_code(201);
+                            http_response_code(200);
                             echo $resolve  = '{
                                 "signup": "true",
                                 "message": "User account succesfully created."
