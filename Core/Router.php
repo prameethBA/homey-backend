8c93328ffbb88dda3b46ad837e0c0ca549875c21
Dimuthu Lakmal <39994919+Lakmal98@users.noreply.github.com>
2020-11-02 16:45:11 +0530

    Aloow * for acces control origin

--- Core/Router.php
+++ Core/Router.php
@@ -3,16 +3,16 @@
 namespace Core;
 
 // Allow from any origin
-if(isset($_SERVER["HTTP_ORIGIN"]))
-{
-    // You can decide if the origin in $_SERVER['HTTP_ORIGIN'] is something you want to allow, or as we do here, just allow all
-    header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
-}
-else
-{
+// if(isset($_SERVER["HTTP_ORIGIN"]))
+// {
+//     // You can decide if the origin in $_SERVER['HTTP_ORIGIN'] is something you want to allow, or as we do here, just allow all
+//     header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+// }
+// else
+// {
     //No HTTP_ORIGIN set, so we allow any. You can disallow if needed here
     header("Access-Control-Allow-Origin: *");
-}
+// }
 
 header("Access-Control-Allow-Credentials: true");
 header("Access-Control-Max-Age: 600");    // cache for 10 minutes
