bd65f04875cc16507c19651cd05596ab5f174faa
Dimuthu Lakmal <39994919+Lakmal98@users.noreply.github.com>
2020-11-15 18:49:03 +0530

    Update CORS policy

--- Core/Router.php
+++ Core/Router.php
@@ -20,7 +20,7 @@ header("Access-Control-Max-Age: 600");    // cache for 10 minutes
 if($_SERVER["REQUEST_METHOD"] == "OPTIONS")
 {
     if (isset($_SERVER["HTTP_ACCESS_CONTROL_REQUEST_METHOD"]))
-        header("Access-Control-Allow-Methods: POST, GET, OPTIONS, DELETE, PUT"); //Make sure you remove those you do not want to support
+        header("Access-Control-Allow-Methods: POST, GET, OPTIONS, DELETE, PUT, PATCH"); //Make sure you remove those you do not want to support
 
     if (isset($_SERVER["HTTP_ACCESS_CONTROL_REQUEST_HEADERS"]))
         header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
