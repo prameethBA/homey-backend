9a6d14385ffdbcd971d490937110215d847edad0
Lakmal98 <lakmalepp@gmail.com>
2021-03-19 19:40:09 +0530

    add new models

--- Controllers/PropertyUpdate.php
+++ Controllers/PropertyUpdate.php
@@ -13,6 +13,10 @@ require_once('Models/Property.php');
 
 use Models\Property as PropertyModel;
 
+require_once('Models/ServiceFees.php');
+
+use Models\ServiceFees as ServiceFees;
+
 require_once('Models/PropertySettings.php');
 
 use Models\PropertySettings as PropertySettings;
@@ -25,42 +29,43 @@ require_once('Core/DB/DB.php');
 
 use Core\DB\DB as DB;
 
-class Property extends BaseController
+class PropertyUpdate extends BaseController
 {
 
     public function __construct($params, $secureParams)
     {
         parent::__construct($params, $secureParams);
         new PropertyModel();
-        new PropertySettings();
-        new Favourite();
+        new ServiceFees();
+        // new PropertySettings();
+        // new Favourite();
     }
 
     public function get()
     {
         try {
             if (isset($this->params[0])) {
-                switch ($this->params[0]) {
-                    case 'all':
-                        $stmt = DB::execute(PropertyModel::join('*', ("INNER JOIN propertysettings ON property._id = propertysettings.property_id WHERE property.privated = 0 AND property.property_status = 1 ORDER BY property.created DESC")));
-                        // $stmt = DB::execute(PropertyModel::get(['_id', 'title', 'price', 'description'], (int)$this->params[1], (int)$this->params[1] * (int)$this->params[2]));
-                        http_response_code(200);
-                        echo $resolve = json_encode($stmt->fetchAll());
-                        break;
-                    case 'search':
-                        $stmt = DB::execute(PropertyModel::join('*', ("INNER JOIN propertysettings ON property._id = propertysettings.property_id WHERE (property.title LIKE '%{$this->params[1]}%' OR property.description LIKE '%{$this->params[1]}%') AND property.privated = 0 AND property.property_status = 1 ORDER BY property.created DESC")));
-                        // $stmt = DB::execute(PropertyModel::get(['_id', 'title', 'price', 'description'], (int)$this->params[1], (int)$this->params[1] * (int)$this->params[2]));
-                        http_response_code(200);
-                        echo $resolve = json_encode($stmt->fetchAll());
-                        break;
-                    default:
-                        http_response_code(200);
-                        die($reject = '{
-                                "status": "400",
-                                "message": "Invalid request."
-                        }');
-                        break;
-                }
+                // switch ($this->params[0]) {
+                //     case 'all':
+                //         $stmt = DB::execute(PropertyModel::join('*', ("INNER JOIN propertysettings ON property._id = propertysettings.property_id WHERE property.privated = 0 AND property.property_status = 1 ORDER BY property.created DESC")));
+                //         // $stmt = DB::execute(PropertyModel::get(['_id', 'title', 'price', 'description'], (int)$this->params[1], (int)$this->params[1] * (int)$this->params[2]));
+                //         http_response_code(200);
+                //         echo $resolve = json_encode($stmt->fetchAll());
+                //         break;
+                //     case 'search':
+                //         $stmt = DB::execute(PropertyModel::join('*', ("INNER JOIN propertysettings ON property._id = propertysettings.property_id WHERE (property.title LIKE '%{$this->params[1]}%' OR property.description LIKE '%{$this->params[1]}%') AND property.privated = 0 AND property.property_status = 1 ORDER BY property.created DESC")));
+                //         // $stmt = DB::execute(PropertyModel::get(['_id', 'title', 'price', 'description'], (int)$this->params[1], (int)$this->params[1] * (int)$this->params[2]));
+                //         http_response_code(200);
+                //         echo $resolve = json_encode($stmt->fetchAll());
+                //         break;
+                //     default:
+                //         http_response_code(200);
+                //         die($reject = '{
+                //                 "status": "400",
+                //                 "message": "Invalid request."
+                //         }');
+                //         break;
+                // }
             }
         } catch (Exception $err) {
             http_response_code(200);
@@ -77,148 +82,89 @@ class Property extends BaseController
             if (isset($this->params[0])) {
                 if (!$this->authenticate()) throw new Exception("Unautherized request.");
                 switch ($this->params[0]) {
-                    case 'add-new':
+                    case 'get':
 
+                        // $userId = $this->secureParams['userId'];
+                        // $token = $this->secureParams['token'];
+
+
+                        // if ($this->authenticateUser($userId, $token)) {
+                        //     $id = $this->uniqueKey($userId);
+                        //     $location = json_encode($this->secureParams['location']);
+                        //     $facilities = json_encode($this->secureParams['facilities']);
+
+                        //     $data = [
+                        //         '_id' => $id,
+                        //         'user_id' => $this->secureParams['userId'],
+                        //         'title' => $this->secureParams['title'],
+                        //         'location' => $location,
+                        //         'rental_period' => $this->secureParams['rentalperiod'],
+                        //         'price' => (int)$this->secureParams['price'],
+                        //         'key_money' => (int)$this->secureParams['keyMoney'],
+                        //         'minimum_period' => (int)$this->secureParams['minimumPeriod'],
+                        //         'available_from' => $this->secureParams['availableFrom'],
+                        //         'property_type_id' => $this->secureParams['propertyType'],
+                        //         'description' => $this->secureParams['description'],
+                        //         'district_id' => $this->secureParams['district'], //This is unnecceary, can be removed
+                        //         'city_id' => $this->secureParams['city'],
+                        //         'facilities' => $facilities
+                        //     ];
+
+                        //     $stmt = DB::execute(PropertyModel::save($data));
+
+                        //     // save images
+                        //     if (isset($this->secureParams['images'])) {
+
+                        //         $path  = $_SERVER["DOCUMENT_ROOT"] . "/data/propertyImages/" . $id;
+
+                        //         // Make a folder for each property with property ID
+                        //         if ($this->makeDir($path, 0777, false)) {
+                        //             // Save each image for the created directory
+                        //             $index = 1;
+                        //             foreach ($this->secureParams['images'] as $img) {
+                        //                 // if file not saved correctly throw an error
+                        //                 if (!$this->base64ToImage($img, $path . "/" . $index++)) {
+                        //                     http_response_code(200);
+                        //                     die($reject = '{
+                        //                         "status": "424",
+                        //                         "error": "true",
+                        //                         "message": "Failed to put images into database"
+                        //                         }
+                        //                     }');
+                        //                 }
+                        //             }
+                        //         } else throw new Exception("Permission Denied. Server side failure.");
+                        //     } //End of save images
+                        //     http_response_code(201);
+                        //     echo $resolve = '{
+                        //         "action": "true",
+                        //         "propertyId": "' . $id . '",
+                        //         "message": "The advertisement saved successfully."
+                        //     }
+                        //     ';
+                        // } else throw new Exception("Authentication failed. Unauthorized request.");
+
+                        break;
+
+                    case 'add':
                         $userId = $this->secureParams['userId'];
                         $token = $this->secureParams['token'];
-
-
                         if ($this->authenticateUser($userId, $token)) {
-                            $id = $this->uniqueKey($userId);
-                            $location = json_encode($this->secureParams['location']);
-                            $facilities = json_encode($this->secureParams['facilities']);
+                            $stmt = DB::execute(PropertyModel::get('key_money as keyMoney, title', ("_id = '" . $this->secureParams['propertyId'] . "'")));
+                            
+                            $results = $stmt->fetch();
 
-                            $data = [
-                                '_id' => $id,
-                                'user_id' => $this->secureParams['userId'],
-                                'title' => $this->secureParams['title'],
-                                'location' => $location,
-                                'rental_period' => $this->secureParams['rentalperiod'],
-                                'price' => (int)$this->secureParams['price'],
-                                'key_money' => (int)$this->secureParams['keyMoney'],
-                                'minimum_period' => (int)$this->secureParams['minimumPeriod'],
-                                'available_from' => $this->secureParams['availableFrom'],
-                                'property_type_id' => $this->secureParams['propertyType'],
-                                'description' => $this->secureParams['description'],
-                                'district_id' => $this->secureParams['district'], //This is unnecceary, can be removed
-                                'city_id' => $this->secureParams['city'],
-                                'facilities' => $facilities
-                            ];
-
-                            $stmt = DB::execute(PropertyModel::save($data));
-
-                            // save images
-                            if (isset($this->secureParams['images'])) {
-
-                                $path  = $_SERVER["DOCUMENT_ROOT"] . "/data/propertyImages/" . $id;
-
-                                // Make a folder for each property with property ID
-                                if ($this->makeDir($path, 0777, false)) {
-                                    // Save each image for the created directory
-                                    $index = 1;
-                                    foreach ($this->secureParams['images'] as $img) {
-                                        // if file not saved correctly throw an error
-                                        if (!$this->base64ToImage($img, $path . "/" . $index++)) {
-                                            http_response_code(200);
-                                            die($reject = '{
-                                                "status": "424",
-                                                "error": "true",
-                                                "message": "Failed to put images into database"
-                                                }
-                                            }');
-                                        }
-                                    }
-                                } else throw new Exception("Permission Denied. Server side failure.");
-                            } //End of save images
-                            http_response_code(201);
-                            echo $resolve = '{
-                                "action": "true",
-                                "propertyId": "' . $id . '",
-                                "message": "The advertisement saved successfully."
-                            }
-                            ';
+                            $result['title'] = $results['title'];
+                            $result['keyMoney'] = $results['keyMoney'];
+                            
+                            $stmt = DB::execute(ServiceFees::get('fee', ("service = 'reserve'")));
+                            $result['fee'] = $stmt->fetch()['fee'];
+                            http_response_code(200);
+                            echo json_encode($result);
                         } else throw new Exception("Authentication failed. Unauthorized request.");
-
                         break;
 
-                    case 'get':
-                        switch ($this->params[1]) {
-                            case 'property':
-                                $userId = $this->secureParams['userId'];
-                                $token = $this->secureParams['token'];
-                                if ($this->authenticateUser($userId, $token)) {
-                                    $stmt = DB::execute(PropertyModel::get('*', ("_id = '" . $this->secureParams['propertyId'] . "'")));
-                                    http_response_code(200);
-                                    echo json_encode($stmt->fetch());
-                                } else throw new Exception("Authentication failed. Unauthorized request.");
-                                break;
 
-                            case 'own':
-                                // $stmt = DB::execute(PropertyModel::join('*', ("INNER JOIN propertysettings ON property._id = propertysettings.property_id WHERE property._id = '{$this->secureParams['propertyId']}'")));
-                                $stmt = DB::execute(PropertyModel::join('*', ("INNER JOIN propertysettings ON property._id = propertysettings.property_id WHERE property.user_id = '{$this->secureParams['userId']}' ORDER BY property.created DESC")));
-                                http_response_code(200);
-                                echo json_encode($stmt->fetchAll());
-                                break;
-
-                            default:
-                                throw new Exception("Authentication failed. Unauthorized request.");
-                        }
-
-                        break;
-                    case 'remove':
-                        DB::exec(PropertyModel::delete("_id = '{$this->secureParams['propertyId']}'"));
-                        DB::exec(PropertySettings::delete("property_id = '{$this->secureParams['propertyId']}'"));
-                        http_response_code(200);
-                        echo $resolve = '{
-                                    "status": "204",
-                                    "message": "Property Removed"
-                                }';
-                        break;
-
-                    case 'favourite':
-                        switch ($this->params[1]) {
-                            case 'get':
-                                $stmt = DB::execute(Favourite::get('COUNT(_id) as count', ("user_id = {$this->secureParams['userId']} AND property_id = '{$this->secureParams['propertyId']}'")));
-
-                                http_response_code(200);
-                                echo ('{
-                                    "action": "' . $stmt->fetch()['count'] . '",
-                                    "message": "retived"
-                                }');
-                                break;
-
-                            case 'getAll':
-                                $userId = $this->secureParams['userId'];
-                                $token = $this->secureParams['token'];
-                                if ($this->authenticateUser($userId, $token)) {
-                                    $stmt = DB::execute(PropertyModel::join('*', ("INNER JOIN favourite  ON property._id = favourite.property_id WHERE NOT property.user_id = '" . $this->secureParams['userId'] . "' AND favourite.user_id = '" . $this->secureParams['userId'] . "'")));
-                                    http_response_code(200);
-                                    echo json_encode($stmt->fetchAll());
-                                } else throw new Exception("Authentication failed. Unauthorized request.");
-                                break;
-
-                            case 'add':
-                                $stmt = DB::execute(Favourite::save(['user_id' => $this->secureParams['userId'], 'property_id' => $this->secureParams['propertyId']]));
-                                http_response_code(200);
-                                echo ('{
-                                    "status": "204",
-                                    "message": "Added to favourite"
-                                }');
-                                break;
-
-                            case 'remove':
-                                $stmt = DB::execute(Favourite::delete(("property_id = '{$this->secureParams['propertyId']}' AND user_id = '{$this->secureParams['userId']}'")));
-                                http_response_code(200);
-                                echo ('{
-                                    "status": "204",
-                                    "message": "Remove from favourite"
-                                }');
-                                break;
-
-                            default:
-                                throw new Exception("Authentication failed. Unauthorized request.");
-                        }
-                        break;
                     default:
                         throw new Exception("Invalid parameter");
                 } //End of the switch
