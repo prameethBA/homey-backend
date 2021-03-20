12bcf9938cf7494cd3aa8a98b734275419273b12
Lakmal98 <lakmalepp@gmail.com>
2021-03-20 14:37:41 +0530

    change modifier

--- Core/Config/PaymentGateway.php
+++ Core/Config/PaymentGateway.php
@@ -4,9 +4,9 @@ namespace Core\Config;
 
 class PaymentGateway
 {
-    protected static $merchantId = "1213639";
-    protected static $returnUrl = "https:://homey.lk/payment/done";
-    protected static $cancelUrl = "https://homey.lk/payment/cancel";
-    protected static $notifyUrl = "https://homey.lk/payment/notify";
-    protected static $currency = "LKR";
+    public static $merchantId = "1213639";
+    public static $returnUrl = "https:://homey.lk/payment/done";
+    public static $cancelUrl = "https://homey.lk/payment/cancel";
+    public static $notifyUrl = "https://homey.lk/payment/notify";
+    public static $currency = "LKR";
 }
