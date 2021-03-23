2ec8cfab07adf5067802de75b7ed4d0a39e36ec9
Prameeth Maduwantha <64423592+prameethBA@users.noreply.github.com>
2021-03-23 09:46:46 +0530

    Update PropertyType.php

--- Controllers/PropertyType.php
+++ Controllers/PropertyType.php
@@ -4,20 +4,11 @@ namespace Controllers;
 
 use Exception;
 
-require_once('Core/BaseController.php');
-use Core\BaseController as BaseController;
-require_once('Models/PropertyType.php');
-use Models\PropertyType as Property;
+require_once('Core/Controller.php');
+use Core\Controller as Controller;
 
-require_once('Core/DB/DB.php');
-use Core\DB\DB as DB;
+class PropertyType extends Controller {
 
-class PropertyType extends BaseController {
-
-    public function __construct($params, $secureParams) {
-        parent::__construct($params, $secureParams);
-        new Property();
-    }
 
     public function get() {
         try {
